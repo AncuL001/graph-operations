1d2c50721796e9ecfaf1930090c7a1c3d756dbe7
AncuL001 <ancul001@gmail.com>
2022-06-06 16:01:15 +0700

    fill graph dynamically

--- main.cpp
+++ main.cpp
@@ -31,23 +31,169 @@ void fillGraph(WeightedGraph<char>& g) {
     g.addEdge('h', 'i', 7);
 }
 
-int main() {
-    WeightedGraph<char> g;
-    fillGraph(g);
 
+template <typename VType>
+void showEdgeListMenu(WeightedGraph<VType>& g) {
+    for (auto edge: g.getAllEdges()) {
+        std::cout << std::get<0>(edge) << " -- "
+                  << std::get<1>(edge) << " : "
+                  << std::get<2>(edge) << "\n";
+    }
+}
+
+template <typename VType>
+void addVertexMenu(WeightedGraph<VType>& g) {
+    while (true) {
+        VType val;
+
+        std::cout << "Masukkan nama vertex (0: Kembali): ";
+        std::cin >> val;
+
+        if (val == '0') return;
+
+        try {
+            g.addVertex(val);
+        }
+        catch(const std::exception& e) {
+            std::cerr << e.what() << '\n';
+            continue;
+        }
+        return;
+    }
+}
+
+template <typename VType>
+void addEdgeMenu(WeightedGraph<VType>& g) {
+    while (true) {
+        VType val1, val2;
+        int weight;
+
+        std::cout << "Masukkan nama vertex 1 (0: Kembali): ";
+        std::cin >> val1;
+        if (val1 == '0') return;
+
+        std::cout << "Masukkan nama vertex 2 (0: Kembali): ";
+        std::cin >> val2;
+        if (val2 == '0') return;
+
+        std::cout << "Masukkan weight (0: Kembali): ";
+        std::cin >> weight;
+        if (weight == 0) return;
+
+        try {
+            g.addEdge(val1, val2, weight);
+        }
+        catch(const std::exception& e) {
+            std::cerr << e.what() << '\n';
+            continue;
+        }
+        return;
+    }
+}
+
+template <typename VType>
+void traversalMenu(WeightedGraph<VType>& g) {
+    std::cout << "1. Kruskal\n"
+              << "2. Prim\n"
+              << "0. Kembali\n"
+              << "Menu: ";
+
+    int op;
+    std::cin >> op;
+
+    switch (op) {
+    case 1:
+        kruskal(g);
+        break;
+    
+    case 2:
+        prim(g);
+        break;
+
+    default:
+        break;
+    }
+}
+
+template <typename VType>
+void kruskal(WeightedGraph<VType>& g) {
     Kruskal kruskal(g);
     for (auto edge: kruskal.getMst()) {
         std::cout << std::get<0>(edge) << " - "
                     << std::get<1>(edge) << " : "
                     << std::get<2>(edge) << '\n';
     }
+}
 
-    std::cout << '\n';
+template <typename VType>
+void prim(WeightedGraph<VType>& g) {
+    VType startVertex;
 
-    Prim prim(g, 'a');
+    while (true) {
+        std::cout << "Vertex pertama (0: Kembali): ";
+        std::cin >> startVertex;
+        if (startVertex == '0') return;
+
+        if (g.edges.find(startVertex) == g.edges.end()) {
+            std::cout << "vertex tidak ditemukan";
+            continue;
+        }
+
+        break;
+    }
+
+    Prim prim(g, startVertex);
     for (auto edge: prim.getMst()) {
         std::cout << std::get<0>(edge) << " - "
                     << std::get<1>(edge) << " : "
                     << std::get<2>(edge) << '\n';
+    }
+}
+
+int main() {
+    WeightedGraph<char> g;
+
+    while (true) {
+        std::cout << "1. Lihat daftar edge\n"
+                  << "2. Tambahkan vertex\n"
+                  << "3. Tambahkan edge\n"
+                  << "4. Traversal\n";
+
+        if (g.edges.size() == 0) 
+            std::cout << "5. Isi graph dengan graph template\n";
+
+        std::cout << "0. Keluar\n"
+                  << "Menu: ";
+
+        int op;
+        std::cin >> op;
+
+        switch (op) {
+        case 1:
+            showEdgeListMenu(g);
+            break;
+
+        case 2:
+            addVertexMenu(g);
+            break;
+
+        case 3:
+            addEdgeMenu(g);
+            break;
+
+        case 4:
+            traversalMenu(g);
+            break;
+
+        case 5:
+            fillGraph(g);
+            break;
+
+        case 0:
+            return 0;
+
+        default:
+            break;
+        }
     }
 }
--- weightedGraph.hpp
+++ weightedGraph.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <unordered_map>
+#include <stdexcept>
 #include <vector>
 #include <set>
 #include <tuple>
@@ -11,15 +12,22 @@ public:
     std::unordered_map<VType, std::unordered_map<VType, int>> edges;
 
     void addVertex(VType val) {
+        if (edges.find(val) != edges.end())
+            throw std::invalid_argument( "vertex sudah ada" );
+
         edges.insert(std::make_pair(val, std::unordered_map<VType, int>()));
     }
 
     void addEdge(VType val1, VType val2, int weight) {
-        if (edges.find(val1) != edges.end() &&
-            edges.find(val2) != edges.end()) {
+        if (weight <= 0) throw std::invalid_argument( "weight tidak bisa negatif" );
+        if (edges.find(val1) == edges.end() || edges.find(val2) == edges.end()) 
+            throw std::invalid_argument( "vertex tidak ditemukan" );
+
+        if (edges.find(val1) != edges.end() && edges[val1].find(val2) != edges[val1].end() )
+            throw std::invalid_argument( "edge sudah ada" );
+
         edges[val1].insert(std::make_pair(val2, weight));
         edges[val2].insert(std::make_pair(val1, weight));
-        }
     }
 
     std::vector<std::tuple<VType, VType, int>> getAllEdges() {
